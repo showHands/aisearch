a83f4142f555b21a012c9e666f40c690b9580f53
wangxiaotao <13269639602@163.com>
2018-12-24 14:16:27 +0800

    项目基础信息整合： mybatis 逆向工程 日志打印 redis缓存 swagger 分页 跨站点脚本编制 支持跨域请求

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # aisearch
+
+**_redis缓存_**
+
+springboot 缓存基础知识讲解：https://blog.csdn.net/qq_32867467/article/details/82944506
+springboot 集成redis做缓存： https://www.jianshu.com/p/2513f625fcae
+
+
+**_swagger2_**:
+
+http://www.cnblogs.com/paddix/p/8204916.html
+Swagger是世界上最流行的API工具，并且Swagger的目的是支撑整个API生命周期的开发，包括设计、文档以及测试和部署。
+项目集成Swaggerh后，只需要在代码上加上swagger注解，就可以自动生成API说明文档。减轻了维护接口文档的工作，对于
+整个项目后续的维护来说即省时又省力
+
+项目启动后Swagger的访问路径：http://127.0.0.1:8080/swagger-ui.html
+
+
+**_日志管理：_**
+
+https://blog.csdn.net/ws_dj_love/article/details/80227486
+logback-spring.xml,实现了通过读取配置指定日志输出路径并且支持打印sql脚本
+
+
+**_mybatis逆向工程：_**
+resource/generatorConfig.xml
+resource/mybatisGeneratorinit.properties
+
+分页：
+PageHelper
+
+**_AOP实现日志拦截：_**
+支持打印请求方法、参数及响应结果。
+
+**_跨站点脚本编制_**
+XssFilter
+
+**_支持跨站请求_**
+CrossFilter, 重写response
+
+**_打包方式：_**
+java jar aisearch0.0.1.jar
+注：打包前将pom.xml中的逆向工程配置注释掉，否则会重复生成generatorConfig.xml中的实体类、*Mapper.xml
+
+
+
+
+
+
--- src/main/java/com/hengan/aisearch/controller/UserController.java
+++ src/main/java/com/hengan/aisearch/controller/UserController.java
@@ -3,16 +3,16 @@ package com.hengan.aisearch.controller;
 import com.hengan.aisearch.db.schema.AIUser;
 import com.hengan.aisearch.service.AIUserService;
 import com.hengan.aisearch.vo.ResponseBean;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
  * @Classname UserController
@@ -29,9 +29,9 @@ public class UserController {
     AIUserService aiUserService;
 
     @ApiOperation(value = "根据用户id查询用户实体信息", notes = "查询用户实体信息")
-    @ApiImplicitParam(name = "userId", value = "用户ID", paramType = "path", required = true, dataType ="Long")
+    @ApiImplicitParam(name = "userId", value = "用户ID")
     @GetMapping("/queryUserById")
-    public ResponseBean queryUserById(@RequestParam("userId") Long userId){
+    public ResponseBean queryUserById(@RequestParam(value = "userId", defaultValue = "-1") final Long userId){
         AIUser aiUser = aiUserService.queryByUserId(userId);
         return ResponseBean.successdata(aiUser);
     }
@@ -61,8 +61,21 @@ public class UserController {
 
     }
 
-    @GetMapping("/hello")
-    public String hello(){
-        return "hello world";
+    /**
+     * demo
+     * @param object
+     * @return
+     */
+    @RequestMapping(value = "/article", method = POST, produces = "application/json;charset=utf-8")
+    @ApiOperation(value = "添加文章", notes = "添加新的文章", tags = "Article",httpMethod = "POST")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "title", value = "文章标题", required = true, dataType = "String"),
+            @ApiImplicitParam(name = "summary", value = "文章摘要", required = true, dataType = "String"),
+            @ApiImplicitParam(name = "status", value = "发布状态", required = true, dataType = "Integer")
+    })
+    @ApiResponse(code=200,message="成功",response=ResponseBean.class)
+    public ResponseBean saveArticle(@RequestBody Object object){
+
+        return ResponseBean.success("查询成功");
     }
 }
